b20548918f7a2f9ee1ede0afca5c9c1ece8e98f9
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-27 18:24:59 -0600

    completed console exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,76 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %f", pi);
+//        System.out.print("Enter a word: ");
+//        String userInput = scanner.next();
+//        System.out.print("Enter three (3) words.");
+//        String userInput = scanner.nextLine(); // enter more than one string
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        Declarations
+//        String string1;
+//        String string2;
+//        String string3;
+////        Prompts
+//        System.out.println("Enter three words.");
+//        System.out.println("Separate each with a return.");
+////        Read in values
+//        string1 = scanner.nextLine();
+//        string2 = scanner.nextLine();
+//        string3 = scanner.next();
+//        System.out.println("You entered: ");
+//        System.out.println(string1 + ", " + string2 + " and " + string3);
+//        String sentence;
+//        System.out.println("Enter a sentence.");
+////        sentence = scanner.next(); //only returns first word
+//        sentence = scanner.nextLine();
+//        System.out.println("You entered: ");
+//        System.out.println(sentence);
+//        String length;
+//        String width;
+//        String height;
+//        System.out.println("Finding area and perimeter...");
+//        System.out.println("Enter a length, width and height. Do not put units.");
+//        length = scanner.nextLine();
+//        int length = Integer.parseInt(length.trim());
+//        width = scanner.nextLine();
+//        int width = Integer.parseInt(width.trim());
+//        height = scanner.nextLine();
+//        int height = Integer.parseInt(height.trim());
+//        System.out.println("The area is: ");
+//        System.out.println(length * width);
+//        System.out.println("The perimeter is: ");
+//        System.out.println(2* length + 2 * width);
+//        System.out.println("The volume is: ");
+//        System.out.println(length * width * height);
+//        double length;
+//        double width;
+//        double height;
+//        System.out.println("Finding area and perimeter...");
+//        System.out.println("Enter a length, width and height. Do not put units.");
+//        length = scanner.nextDouble();
+//        scanner.useDelimiter("\n");
+////        length = Double.parseDouble(length);
+//        width = scanner.nextDouble();
+//        scanner.useDelimiter("\n");
+////        width = Double.parseDouble(width);
+//        height = scanner.nextDouble();
+//        scanner.useDelimiter("\n");
+////        height = Double.parseDouble(height);
+//        System.out.println("The area is: ");
+//        System.out.println(length * width);
+//        System.out.println("The perimeter is: ");
+//        System.out.println(2* length + 2 * width);
+//        System.out.println("The volume is: ");
+//        System.out.println(length * width * height);
+    }
 }
+
+/*
+System.out.format("The value of " + "the float variable is " +
+     "%f, while the value of the " + "integer variable is %d, " +
+     "and the string is %s", floatVar, intVar, stringVar);
+*/
