93ab4a04d5bcdd655c0b25deb6db1e16ee7bfa27
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-29 11:24:51 -0600

    completed string and bob exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,13 +1,25 @@
 public class StringExercise {
     public static void main(String[] args) {
 
-
-
+        String pinkFloyd = "We don't need no education\nWe don't need no thought control\n";
+        String quotes ="Check \"this\" out!, \"s inside of \"s!\n";
+        String driveC = "In windows, the main drive is usually C:\\\n";
+        String backslash = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!\n";
+        System.out.println(pinkFloyd);
+        System.out.println(quotes);
+        System.out.println(driveC);
+        System.out.println(backslash);
     }
 }
 
 /*
 
+// STRING COMPARISON METHODS
 
+method	            description
+.equals	            determine, case-sensitively, if two strings have the same value.
+.equalsIgnoreCase	determine if two strings have the same value, disregarding their casing.
+.startsWith	        determine, case-sensitively, if one string starts with another.
+.endsWith	        determine, case-sensitively, if one string ends with another.
 
  */
