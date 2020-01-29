e6cb57ab87ad3062ea31bb6b45728f6f303bbad5
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-29 16:03:41 -0600

    completed dice exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -11,7 +11,8 @@ public class MethodsExercises {
         System.out.println(multiAddition(7, 8));
         System.out.println(multiAdditionRecursive(7, 8));
 //        System.out.println(getInteger(1, 10));
-        System.out.println(factorial());
+//        System.out.println(factorial());
+        System.out.println(dice());
     }
 
     public static int addition(int num1, int num2) {
@@ -61,27 +62,54 @@ public class MethodsExercises {
 //        return userInput;
 //    }
 
-    public static long factorial() {
-        Scanner scanner = new Scanner(System.in);
-        int recalculate;
-        do {
-            System.out.print("Enter an integer between 1 and 10: ");
+//    //long can store up to 2^63 bytes; maximum value is 49 because 50! has 10^64
+//    public static long factorial() {
+//        Scanner scanner = new Scanner(System.in);
+//        long recalculate;
+//        do {
+//            System.out.print("Enter an integer between 1 and 50: ");
+//
+//            long userInput = scanner.nextInt();
+//            long total = 1;
+//            for (long i = 1; i < userInput; i++) {
+//                if (userInput < 50) {
+//                    total *= i;
+//                } else {
+//                    System.out.println("Please enter a valid number");
+//                    return factorial();
+//                }
+//            }
+//            System.out.print(userInput + "! total is: " + (userInput * total));
+//            System.out.println("\nEnter 1 to calculate another factorial, or 0 to exit.");
+//            recalculate = scanner.nextInt();
+//        } while (recalculate == 1);
+//        return recalculate;
+//    }
 
-            int userInput = scanner.nextInt();
-            int total = 1;
-            for (int i = 1; i < userInput; i++) {
-                if (userInput < 10) {
-                    total *= i;
-                } else {
-                    System.out.println("Please enter a valid number");
-                    return factorial();
-                }
-            }
-            System.out.print(userInput + "! total is: " + (userInput * total));
-            System.out.println("\nEnter 1 to calculate another factorial, or 0 to exit.");
-            recalculate = scanner.nextInt();
-        } while (recalculate == 1);
-        return recalculate;
+    public static int dice() {
+        Scanner scanner = new Scanner(System.in);
+        int sides;
+        int ready;
+        int die1;
+        int die2;
+        int reroll;
+
+        do {
+            System.out.println("Let's roll a pair of dice!\n");
+            System.out.println("Enter the number of sides on the die: ");
+            sides = scanner.nextInt();
+            System.out.println("\nReady to roll? Enter 1 for yes and 0 to exit.");
+            ready = scanner.nextInt();
+            die1 = (int) (Math.random() * sides) + 1;
+            die2 = (int) (Math.random() * sides) + 1;
+            System.out.println("First die: " + die1);
+            System.out.println("Second die: " + die2);
+            System.out.println("Total: " + (die1 + die2));
+            System.out.println("\nWould you like to roll again? Enter 1 for yes and 0 to exit.");
+            reroll = scanner.nextInt();
+        } while (reroll == 1);
+        return reroll;
     }
+
 
 }
