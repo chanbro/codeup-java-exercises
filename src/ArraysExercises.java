4a94151492e5ae709329723481ec52e965d5f4fd
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-03 13:47:21 -0600

    completed server generator exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -6,23 +6,25 @@ public class ArraysExercises {
         int[] numbers = {1, 2, 3, 4, 5};
         // Arrays are an object that need to be converted into string to show on command line
         System.out.print(Arrays.toString(numbers));
+        System.out.print("\n");
 
 
         String[] people = new String[]{"Max", "Sally", "Joe"};
+        String[] colors = new String[]{"Blue", "red", "green", "purple", "pink"};
         for (String person : people) {
-                    System.out.println("\n" + person);
+                    System.out.println(person);
         }
-        addPerson(people, "Jeff");
+        addObject(colors, "yellow");
     }
 
     // Exercise 1
-    public static void addPerson(String[] array, String name){
+    public static void addObject(String[] array, String newObject){
         // newArray has a length of the passed array length plus 1
         String[] newArray = new String[array.length +1];
         // Object source (what to copy)= array; Source position (start position to be copied)= 0, Object destination (where to copy to) = newArray, Source destination position (end position to be copied) = (the original) array.length
         System.arraycopy(array, 0, newArray, 0, array.length);
         // Creating object in the array to be copied over into the last position of newArray
-        newArray[newArray.length -1] = name;
+        newArray[newArray.length -1] = newObject;
         // Printing the array
         System.out.println(Arrays.toString(newArray));
         // Printing each object from newArray as a string
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+
 public class ServerNameGenerator {
+    public static void main(String[] args) {
+        ServerNameGenerator server = new ServerNameGenerator();
+        String[] adjectives = {"grandiose", "sulky", "infamous", "striped", "cruel", "careless", "statuesque", "permissible", "previous", "overrated"};
+
+        String[] nouns = {"instruction", "bathroom", "error", "strategy", "awareness", "connection", "explanation", "depression", "method", "soup"};
+
+        System.out.println(server.nameGenerator(adjectives, nouns));
+    }
+
+   private int randomAdj = (int) (Math.random() * 9);
+    private int randomNoun = (int) (Math.random() * 9);
+
+    public String nameGenerator(String[] adj, String[] noun){
+        return adj[randomAdj] + "-" + noun[randomNoun];
+    }
+
 }
