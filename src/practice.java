9f4d324ffc217b366ac538e7a12c5540d1aa8635
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-02 20:14:11 -0600

    added file for practice

---
+++ src/practice.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+//       // OOP Exercise 1
+//        practice person = new practice("Test");
+//        System.out.println(person.getName());
+//        person.setName("John");
+//        person.sayHello();
+
+        // OOP Exercise 2
+
+    }
+
+    // OOP Exercise 2
+    private Scanner scanner;
+
+
+
+//    // OOP Exercise 1
+//    private String name;
+//
+//    public practice(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void sayHello() {
+//        System.out.println("Hello, " + this.name);
+//    }
+
+}
