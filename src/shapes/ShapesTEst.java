c0f16ee5e8a3c5eb7f2353f84f5cdaab66c7bb85
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-04 15:36:47 -0600

    added solutions

--- src/shapes/ShapesTEst.java
+++ src/shapes/ShapesTEst.java
@@ -2,11 +2,11 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4,5);
+        Rectangle box1 = new Rectangle(5,4);
         System.out.println("Area: " + box1.getArea());
         System.out.println("Perimeter: " + box1.getPerimeter());
 
-        Square box2 = new Square(5); // Creating new instance of Square; calls Square methods extended from Rectangle
+        Rectangle box2 = new Square(5); // Creating new instance of Square; calls Square methods extended from Rectangle
         System.out.println("Area: " + box2.getArea());
         System.out.println("Perimeter: " + box2.getPerimeter());
     }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,14 +3,16 @@ package shapes;
 public class Square extends Rectangle {
 
     public Square(double side){
-        super(side, side);
+        super(side, side); // pass side twice into rectangle's "length" and "width"
     }
 
     public double getArea(double side){
+        System.out.println("Square Area: "); // distinguishes Square method from Rectangle
         return side * side;
     }
 
     public double getPerimeter(double side) {
+        System.out.println("Square Perimeter: ");  // distinguishes Square method from Rectangle
         return 4 * side;
     }
 
